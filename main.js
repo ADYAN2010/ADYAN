65da34ebe88683a2fafeb9bf20de3ba84acb9d16
Adyan <106177764+ADYAN2010@users.noreply.github.com>
2025-06-23 23:29:09 +0600

    main.js

--- main.js
+++ main.js
@@ -1,6 +1,10 @@
-AOS.init();
+AOS.init({
+  duration: 1000,
+  once: true,
+  easing: 'ease-in-out',
+});
 
-// Optional Typing Effect
+// Typing Effect
 const title = "Husam Aldin Adyan";
 let i = 0;
 
@@ -8,7 +12,19 @@ function type() {
   if (i < title.length) {
     document.getElementById("typeLine").innerHTML = title.substring(0, i + 1);
     i++;
-    setTimeout(type, 100);
+    setTimeout(type, 120);
   }
 }
-type();
+
+window.onload = type;
+
+// Aesthetic Neon Cursor
+const cursor = document.createElement('div');
+cursor.classList.add('neon-cursor', 'pulse');
+document.body.appendChild(cursor);
+
+// Smooth follow
+document.addEventListener('mousemove', (e) => {
+  cursor.style.top = `${e.clientY}px`;
+  cursor.style.left = `${e.clientX}px`;
+});
